26e75e019f9413e3bd94261c00289479539617ef
Al-Ghozy03 <muhammadfaizalghozi1@gmail.com>
2025-11-08 22:27:58 +0700

    add: class validator

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: false,
+      transform: true,
+    }),
+  );
+  app.setGlobalPrefix('api');
+  await app.listen(3001);
 }
 bootstrap();
