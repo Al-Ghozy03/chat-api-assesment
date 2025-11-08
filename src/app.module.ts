08a58d94ce839c2cc2ce293ee40cefdad9966bc1
Al-Ghozy03 <muhammadfaizalghozi1@gmail.com>
2025-11-08 22:28:34 +0700

    config: env and jwt

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SupabaseModule } from './supabase/supabase.module';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './app/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [SupabaseModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    SupabaseModule,
+    UsersModule,
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SIGN,
+      signOptions: { expiresIn: '30d' },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
   exports: [],
